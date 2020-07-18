e84e2b328b19dcc69552890b79a2370da82d06f4
karthees05 <karthees1985@gmail.com>
2020-07-18 19:30:56 +0100

    gradle jmeter integration

--- module01-examples/module01-question01/src/main/java/com/spring/professional/exam/tutorial/module01/question01/no/dependency/injection/Runner.java
+++
@@ -1,9 +0,0 @@
-package com.spring.professional.exam.tutorial.module01.question01.no.dependency.injection;
-
-public class Runner {
-    public static void main(String... args) {
-        EmployeesSalariesReportService employeesSalariesReportService = new EmployeesSalariesReportService();
-
-        employeesSalariesReportService.generateReport();
-    }
-}
